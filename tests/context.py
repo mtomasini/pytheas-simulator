62a86196e4381a501e880e8f373f0fa3f900c254
Matteo Tomasini <matteo.tomasini@lir.gu.se>
2024-06-27 12:01:53 +0200

    Started to write tests for Boat

--- tests/context.py
+++ tests/context.py
@@ -5,3 +5,11 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytheas
+
+boat = pytheas.boat.Boat(
+        craft = "Hjortspring",
+        latitude = 0,
+        longitude = 0,
+        target = [1,1] 
+    )
+print(boat.craft)
--- tests/tests_boat.py
+++ tests/tests_boat.py
@@ -1,1 +1,23 @@
 from .context import pytheas
+
+def test_calculate_displacement():
+    pass
+
+def test_generic_displacement():
+    pass
+
+def test_move_boat():
+    boat = pytheas.boat.Boat(
+        craft = "Hjortspring",
+        latitude = 0,
+        longitude = 0,
+        target = [1,1] 
+    )
+    
+    assert len(boat.trajectory) == 1
+    assert boat.trajectory == [(0, 0)]
+    
+    # if winds and currents are 0, there should be no movement, but still append a point
+    boat.move_boat([0, 0], [0, 0])
+    assert len(boat.trajectory)
+    assert boat.trajectory[-1] == [0, 0]
