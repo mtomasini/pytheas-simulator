d77eb40c4f274b42d743b441fdfea3c183a9d62e
Matteo Tomasini <matteo.tomasini@lir.gu.se>
2024-07-12 15:20:55 +0200

    Map loading tested

--- tests/test_map.py
+++ tests/test_map.py
@@ -1,11 +1,13 @@
 import pandas as pd
 from pytheas.map import Map
 
-WIND_DATA = "/media/mtomasini/LaCie/LIR/Full_data/winds_cerra/"
-CURRENT_DATA = "/media/mtomasini/LaCie/LIR/Full_data/currents_northwest/"
-WAVES_DATA = "/media/mtomasini/LaCie/LIR/Full_data/waves_northwest/"
 
-def test_load_map_data():
+def local_test_load_map_data():
+    # WORKED FINE. CHANGED NAME OF TEST TO AVOID AUTOMATIC TESTS
+    WIND_DATA = "/media/mtomasini/LaCie/LIR/Full_data/winds_cerra/"
+    CURRENT_DATA = "/media/mtomasini/LaCie/LIR/Full_data/currents_northwest/"
+    WAVES_DATA = "/media/mtomasini/LaCie/LIR/Full_data/waves_northwest/"
+    
     bounding_box = [56.3, 5.8, 58.8, 13.1] # bounding_box for the Skagerrak strait
     testing_map = Map(bounding_box = bounding_box, 
                       earliest_time = pd.Timestamp('1995-12-15'),
@@ -17,3 +19,8 @@ def test_load_map_data():
     assert testing_map.winds_data.latitude.max() > bounding_box[2]
     assert testing_map.currents_data.time.max() < pd.Timestamp('1996-01-15')
     assert testing_map.waves_data.time.min() > pd.Timestamp('1995-12-14')
+    
+    assert testing_map.return_local_winds([57.7, 9], pd.Timestamp('1995-12-31')) is not None
+    assert testing_map.return_local_currents([57.7, 9], pd.Timestamp('1995-12-31')) is not None
+    assert testing_map.return_local_waves([57.7, 9], pd.Timestamp('1995-12-31')) is not None
+    
